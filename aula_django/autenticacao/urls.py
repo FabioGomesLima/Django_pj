b053780b8b109ffb35456eb346fc1a520b530262
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-22 10:18:36 -0300

    criacao de uma string/urls vazia

--- aula_django/autenticacao/urls.py
+++ aula_django/autenticacao/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/',   views.cadastro)
+    path('cadastro/',   views.cadastro),
+    path ("", views.auth) # uma url vazia
 ]
--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return HttpResponse('Faça seu cadastro')
+    return HttpResponse('Teste')
+
+def auth(request):
+    return HttpResponse("Você está na autenticação")
