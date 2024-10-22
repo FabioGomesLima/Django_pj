7377027b0b3f2475dd330c85e171d389f0e42d56
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-22 17:28:18 -0300

    enviando dados para o template

--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return render(request, 'cadastro/index.html')
+    nome = 'Fabio'
+    idade = '22'
+    profissao = 'Programador'
+    dicionario = {'nome': nome, #dicionário
+                  'idade': idade, 
+                  'profissao':profissao}
+    
+    return render(request, 'cadastro/index.html', { 'nome': nome, #dicionário
+                                                   'idade':idade,
+                                                   'profissao':profissao
+                                                             })
