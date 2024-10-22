94c5dddca8029dd60dfacebfe1125482e4201729
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-22 19:36:13 -0300

    Acessando estrutura de dados em um template django

--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    nome = 'Fabio'
-    idade = '22'
-    profissao = 'Programador'
-    dicionario = {'nome': nome, #dicionário
-                  'idade': idade, 
-                  'profissao':profissao}
+    pessoa =[ 
+                {'nome':'Fabio ',
+                'idade':'22',
+                'profissao':'Programador'
+                               },
+             {'nome':'Fabio Gomes ',
+                'idade':'22',
+                'profissao':'Front End'
+                               } 
+            ]
     
-    return render(request, 'cadastro/index.html', { 'nome': nome, #dicionário
-                                                   'idade':idade,
-                                                   'profissao':profissao
+    
+    return render(request, 'cadastro/index.html', { 'pessoa':pessoa
                                                              })
