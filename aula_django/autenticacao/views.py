cbc95dc02de8ab64ddc379b3ede7045049d9bdee
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-22 22:38:23 -0300

    exibindo arquivos estaticos

--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from aula_django.settings import BASE_DIR
-import os
+
 
 def cadastro(request):
-    print(os.path.join(BASE_DIR,'templates'))
-    pessoa =[ 
-                {'nome':'Fabio ',
-                'idade':'22',
-                'profissao':'Programador'
-                               },
-             {'nome':'Fabio Gomes ',
-                'idade':'22',
-                'profissao':'Front End'
-                               } 
-            ]
+
     
     
-    return render(request, 'cadastro/index.html',{ 'pessoas':pessoa,
-                                                       'x': 0 
-                                                              })
+    return render(request, 'cadastro/index.html')
