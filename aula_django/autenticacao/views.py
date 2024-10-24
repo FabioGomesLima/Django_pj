6cef6e430bfd9c2494c73f9b859c2de31e1d375b
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-24 12:08:44 -0300

    Enviando valores GET por formulários

--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 
 def cadastro(request):
     erro = request.GET.get('erro')
-    
-    
-    
+    print(erro)
     return render(request, 'cadastro/index.html', {'erro':erro})
+
+
+def valida_formulario(request):
+    nome = request.GET.get('nome')
+    email = request.GET.get('email')
+    return HttpResponse(json.dumps({'nome':nome, 'email':email}))
