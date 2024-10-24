5ba88d55579b7fcd8b577c1961bd88662e91291c
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-24 20:28:48 -0300

    insirindo valores na tabela bd

--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
-
+from .models import Pessoa
 
 def cadastro(request):
     if request.method == "GET":
-   
-     return render(request, 'cadastro/index.html')
+        return render(request, 'cadastro/index.html')
     elif request.method == "POST":
-       nome =  request.POST.get('nome')
-       email = request.POST.get('email')
-       return HttpResponse(json.dumps({'nome':nome,'email':email}))
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
 
+        pessoa = Pessoa(
+            nome=nome,
+            email=email,
+            senha=senha
+        )
+
+        pessoa.save()  # Salva a instância no banco de dados
+        return HttpResponse('Você foi cadastrado')
 
