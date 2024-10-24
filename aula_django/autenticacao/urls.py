645a5413f4c97b663e1a913daa4ea6f187b1ed86
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-24 12:42:27 -0300

    Requisicao para uma mesma url

--- aula_django/autenticacao/urls.py
+++ aula_django/autenticacao/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/',   views.cadastro),
-    path('valida_formulario', views.valida_formulario, name='valida_formulario')
+    path('cadastro/',   views.cadastro, name='cadastro'),
     
+
 
    
 ]
