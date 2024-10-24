2c3a79e6fb1462daf74cbb0c04714fdb6ad0b807
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-24 11:25:15 -0300

    menssagem de erro usando o parametro GET

--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -3,10 +3,8 @@ from django.http import HttpResponse
 
 
 def cadastro(request):
-    nome = request.GET.get('nome')
-    sobrenome = request.GET.get('sobrenome')
-    idade = request.GET.get('idade')
+    erro = request.GET.get('erro')
     
     
-    return render(request, 'cadastro/index.html', {'nome':nome,'sobrenome':sobrenome,'idade':idade
-                                                                                                  })
+    
+    return render(request, 'cadastro/index.html', {'erro':erro})
