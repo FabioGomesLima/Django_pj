d86b5a28e31f1f18744d5b8a3f1005096b0e5490
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-10-24 11:13:37 -0300

    Parametros GET

--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 
 def cadastro(request):
-
+    nome = request.GET.get('nome')
+    sobrenome = request.GET.get('sobrenome')
+    idade = request.GET.get('idade')
     
     
-    return render(request, 'cadastro/index.html')
+    return render(request, 'cadastro/index.html', {'nome':nome,'sobrenome':sobrenome,'idade':idade
+                                                                                                  })
